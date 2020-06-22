d54e92f87feef9a10b1f70ac204b1a5cf96ad7c3
Crowdbotics <team@crowdbotics.com>
2020-06-22 23:20:05 +0000

    Update models

--- backend/charaths_18335/wsgi.py
+++ backend/charaths_18335/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charaths_18335.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charaths_18335.settings")
 
 application = get_wsgi_application()
--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Settings
+
+admin.site.register(Settings)
 
 # Register your models here.
